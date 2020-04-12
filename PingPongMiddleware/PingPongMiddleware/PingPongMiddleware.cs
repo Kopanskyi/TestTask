a9386cbb6561020b13694010d6e35cab2e001211
tkopa <tkopa@softserveinc.com>
2020-04-12 18:03:03 +0300

    PingPongMiddleware

---
+++ PingPongMiddleware/PingPongMiddleware/PingPongMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace PingPongMiddleware
+{
+    public class PingPongMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public PingPongMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var responseVal = "TestTask";
+            context.Request.Headers.TryGetValue("TestTask", out var value);
+
+            if (value == "Ping")
+            {
+                if (context.Response.Headers.TryGetValue("TestTask", out var respValue))
+                {
+                    context.Response.Headers.Remove("TestTask");
+                }
+
+                context.Response.Headers.Add("TestTask", "Pong");
+                responseVal = "Pong";
+            }
+
+            await context.Response.WriteAsync(responseVal);
+        }
+    }
+}
---
+++ PingPongMiddleware/PingPongMiddleware/TestTaskEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace PingPongMiddleware
+{
+    public static class TestTaskEndpointRouteBuilderExtensions
+    {
+        public static IEndpointConventionBuilder MapTestTask(this IEndpointRouteBuilder endpoints, string pattern)
+        {
+            var pipeline = endpoints.CreateApplicationBuilder()
+                .UseMiddleware<PingPongMiddleware>()
+                .Build();
+
+            return endpoints.Map(pattern, pipeline);
+        }
+    }
+}
